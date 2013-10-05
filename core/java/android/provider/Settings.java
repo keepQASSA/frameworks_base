5109ec2891849420a0c56e2da4ca3021ba0bd8fc
maxwen <max.weninger@gmail.com>
2013-10-05 13:15:36 +0200

    [1/3] base: add support for adb over network
    Change-Id: If8354c92a03da501c683638cc69097d287d74816

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16186,6 +16186,12 @@ public final class Settings {
         public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
 
         /**
+         * The TCP/IP port to run ADB on, or 0 for USB
+         * @hide
+         */
+        public static final String QASSA_ADB_PORT = "QASSA_adb_port";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
