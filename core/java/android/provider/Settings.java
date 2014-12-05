0aabe247d1ec60f818a686dc22e658d4b87f2feb
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters [1/2]
    Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5542,6 +5542,12 @@ public final class Settings {
         private static final Validator AUTO_BRIGHTNESS_MIN_VALUE_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
