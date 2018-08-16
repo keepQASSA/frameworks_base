7fd00c3d614aa7a6d98bb27a8fa8c5a4fb87a02f
Surge Raval <Surge1223@gmail.com>
2018-08-16 23:45:32 +0000

    wm: Remove status bar permission checks
    NexusLauncherPrebuilt needs setShelfHeight, remove the permissions
    check, because seriously who gives a f***.
    
    Change-Id: I8c981808bb8a4881843e2e832932de775f145546

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5733,8 +5733,6 @@ public class WindowManagerService extends IWindowManager.Stub
 
     @Override
     public void setShelfHeight(boolean visible, int shelfHeight) {
-        mAtmInternal.enforceCallerIsRecentsOrHasPermission(android.Manifest.permission.STATUS_BAR,
-                "setShelfHeight()");
         synchronized (mGlobalLock) {
             getDefaultDisplayContentLocked().getPinnedStackController().setAdjustedForShelf(visible,
                     shelfHeight);
