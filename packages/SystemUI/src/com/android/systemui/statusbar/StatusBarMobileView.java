4efa07fd17f9942401a2062828be0fc115e51faf
Adarsh-MR <adarshmr1998@gmail.com>
2018-10-19 11:51:38 +0000

    Fix VOLTE icon color on Light statusbar
    Change-Id: I96803c61311e8fafe8a395c0afe03c9a33d07feb
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -269,6 +269,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mMobileType.setImageTintList(color);
         mMobileTypeSmall.setImageTintList(color);
         mMobileRoaming.setImageTintList(color);
+        mVolte.setImageTintList(color);
         mDotView.setDecorColor(tint);
         mDotView.setIconColor(tint, false);
     }
@@ -295,6 +296,7 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mMobileType.setImageTintList(list);
         mMobileRoaming.setImageTintList(list);
         mMobileTypeSmall.setImageTintList(list);
+        mVolte.setImageTintList(list);
         mDotView.setDecorColor(color);
     }
 
