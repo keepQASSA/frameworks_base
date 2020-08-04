4e4aa9ea8a3d10841a466624a3aee451fbae9b42
Advaith Bhat <advaithbhat9@gmail.com>
2020-08-04 17:37:20 +0200

    base: Use Selectable Network Traffic Fonts [1/2]
    Change-Id: Ia136028348c128f5667a1ef94715b0e3d81e3ea9
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6063,6 +6063,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
 
         /**
+         * Network traffic font style
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_FONT_STYLE = "network_traffic_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
