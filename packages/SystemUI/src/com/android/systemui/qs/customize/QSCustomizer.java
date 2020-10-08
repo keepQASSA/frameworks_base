b892c55e50d5bc873fac91be54f740ef60feb542
Lucchetto <lucchetto.tie@live.com>
2020-10-08 22:10:37 +0200

    QSCustomizer: add overscroll spring effect
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -56,6 +56,8 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper;
+
 /**
  * Allows full-screen customization of QS, through show() and hide().
  *
@@ -133,6 +135,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         mScreenLifecycle = screenLifecycle;
         updateNavBackDrop(getResources().getConfiguration());
         updateResources();
+
+        OverScrollDecoratorHelper.setUpOverScroll(mRecyclerView, OverScrollDecoratorHelper.ORIENTATION_VERTICAL);
     }
 
     @Override
