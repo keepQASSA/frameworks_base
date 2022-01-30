8a5f7ea1fba51927faac6b0c76a756493d9d6f6c
Arian <arian.kulmer@web.de>
2022-01-30 22:30:12 +0100

    SystemUI: Don't hide the default stream when adjusting music
    If there are two visible rows and the user touches the default (music)
    row, the other row disappears immediately. Avoid this behaviour by
    tracking the row with which this panel was created and keep showing
    that row if the user adjusts the music stream.
    
    Test: While receiving a call both rows are touchable and the additional
          row does not disappear when adjusting the music stream.
    Change-Id: I4e14a0ea50c5cc41cb279c6fbfc8a7d6e6d1ba61

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -206,6 +206,9 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     private boolean mHasAlertSlider;
 
+    // Variable to track the default row with which the panel is initially shown
+    private VolumeRow mDefaultRow = null;
+
     public VolumeDialogImpl(Context context) {
         mContext =
                 new ContextThemeWrapper(context, R.style.qs_theme);
@@ -976,6 +979,10 @@ public class VolumeDialogImpl implements VolumeDialog,
             mConfigChanged = false;
         }
 
+        if (mDefaultRow == null) {
+            mDefaultRow = getActiveRow();
+        }
+
         initSettingsH();
         mDialog.getViewTreeObserver().addOnComputeInternalInsetsListener(mInsetsListener);
 
@@ -1078,6 +1085,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                     mAllyStream = -1;
                     mMusicHidden = false;
                     tryToRemoveCaptionsTooltip();
+                    mDefaultRow = null;
                     mDialog.getViewTreeObserver().removeOnComputeInternalInsetsListener(
                             mInsetsListener);
                     mController.notifyVisible(false);
@@ -1123,11 +1131,14 @@ public class VolumeDialogImpl implements VolumeDialog,
                 return true;
             }
 
-            if (row.defaultStream) {
+            // if the row is the default stream or the row with which this panel was created,
+            // show it additonally to the active row if it is one of the following streams
+            if (row.defaultStream || mDefaultRow == row) {
                 return activeRow.stream == STREAM_RING
                         || activeRow.stream == STREAM_NOTIFICATION
                         || activeRow.stream == STREAM_ALARM
                         || activeRow.stream == STREAM_VOICE_CALL
+                        || activeRow.stream == STREAM_MUSIC
                         || activeRow.stream == STREAM_ACCESSIBILITY
                         || mDynamic.get(activeRow.stream);
             }
