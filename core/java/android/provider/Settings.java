fe52bf63715c7de40660f09a675b38d8255cf7b6
LibXZR <i@xzr.moe>
2022-02-09 09:58:48 +0800

    Add support for gms switch
    qassa:
    adapt to Q & enabled by default
    
    This is a combination of 2 commits.
    
    commit a2e73ce4f3ac9e890d0ce80b79a227a8bd46c5f5
    Author: TH779 <i@779.moe>
    Date:   Sun Feb 20 18:59:20 2022 +0800
    
        GmsManagerService: Add Google Calendar Sync app to GMS packages list
    
        Signed-off-by: TH779 <i@779.moe>
        Change-Id: I22f9cf49aaef49a0ab679906a784a53a5cc980a0
    
    commit 16729652ed5eb59ab4ef16ee7608bc03c04a455f
    Author: LibXZR <i@xzr.moe>
    Date:   Wed Feb 9 09:58:48 2022 +0800
    
        Add support for gms switch
    
        This allows us to enable or disable Google Mobile Services
    components
        by changing a single setting. Also, it provides basic capability to
        fake gms installation status that reports "not installed" rather
    than
        "disabled", because some apps will show a notification when gms is
        disabled but stay quite when gms is totally unavailable.
    
        It has been designed to support multi-user, which means you can
    toggle
        different gms status for every single user.
    
        Change-Id: I7442eb094cea1a191abce02edbf905fac0fb20ad
        Signed-off-by: LibXZR <i@xzr.moe>
    
    Change-Id: I48fb816cd953b847daddbe74fff1376125b3e6fd
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11629,6 +11629,12 @@ public final class Settings {
         public static final String SPOOF_STORAGE_ENCRYPTION_STATUS = "spoof_storage_encryption_status";
 
         /**
+         * Control whether GMS is enabled for this user.
+         * @hide
+         */
+        public static final String GMS_ENABLED = "gms_enabled";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
