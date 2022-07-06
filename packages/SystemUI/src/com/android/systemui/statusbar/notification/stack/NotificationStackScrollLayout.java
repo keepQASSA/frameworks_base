59bc55b525f3910e7f0e154c4cc9d392a0cdfac6
ghou8s <ghou8s@gmail.com>
2022-07-06 06:21:08 +0000

    SystemUI: enable right "R style" notification headers by default [1/2]
    * why enable center "R style" too, because center_notification_headers as dependency
    
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -555,10 +555,10 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
                 Settings.System.NOTIFICATION_HEADERS, 1, UserHandle.USER_CURRENT) == 1;
 
         mCenterHeaders = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.CENTER_NOTIFICATION_HEADERS, 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.CENTER_NOTIFICATION_HEADERS, 1, UserHandle.USER_CURRENT) == 1;
 
         mRightHeaders = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.RIGHT_NOTIFICATION_HEADERS, 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.RIGHT_NOTIFICATION_HEADERS, 1, UserHandle.USER_CURRENT) == 1;
 
         mSectionsManager =
                 new NotificationSectionsManager(
