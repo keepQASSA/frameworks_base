Palladium-OS/platform_frameworks_base

SystemUI: Allow editing number of tiles in QQS (1/2)

packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -25,7 +25,8 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
-
+import android.os.UserHandle;
+import android.provider.Settings;
 import com.android.internal.logging.UiEventLogger;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
@@ -55,8 +56,7 @@ public class QuickQSPanel extends QSPanel {
     public static final String NUM_QUICK_TILES = "sysui_qqs_count";
     private static final String TAG = "QuickQSPanel";
     // Start it at 6 so a non-zero value can be obtained statically.
-    private static int sDefaultMaxTiles = 6;
-
+    private static int sDefaultMaxTiles; 
     private boolean mDisabledByPolicy;
     private int mMaxTiles;
     protected QSPanel mFullPanel;
@@ -73,7 +73,7 @@ public class QuickQSPanel extends QSPanel {
             UiEventLogger uiEventLogger
     ) {
         super(context, attrs, dumpManager, broadcastDispatcher, qsLogger, mediaHost, uiEventLogger);
-        sDefaultMaxTiles = getResources().getInteger(R.integer.quick_qs_panel_max_columns);
+        sDefaultMaxTiles = Settings.System.getIntForUser(context.getContentResolver(),Settings.System.QQS_ICONS,6,UserHandle.USER_CURRENT);
         applyBottomMargin((View) mRegularTileLayout);
     }
 
@@ -218,7 +218,7 @@ public class QuickQSPanel extends QSPanel {
     private final Tunable mNumTiles = new Tunable() {
         @Override
         public void onTuningChanged(String key, String newValue) {
-            setMaxTiles(parseNumTiles(newValue));
+            setMaxTiles(sDefaultMaxTiles);
         }
     };
 

keepQASSA/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -21,6 +21,8 @@ import static com.android.systemui.util.InjectionInflationController.VIEW_CONTEX
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Rect;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -68,7 +70,7 @@ public class QuickQSPanel extends QSPanel {
             }
             removeView((View) mTileLayout);
         }
-        mDefaultMaxTiles = getResources().getInteger(R.integer.quick_qs_panel_max_columns);
+        mDefaultMaxTiles = Settings.System.getIntForUser(context.getContentResolver(),Settings.System.QQS_ICONS,6,UserHandle.USER_CURRENT);
         mTileLayout = new HeaderTileLayout(context);
         mTileLayout.setListening(mListening);
         addView((View) mTileLayout, 0 /* Between brightness and footer */);
@@ -150,7 +152,7 @@ public class QuickQSPanel extends QSPanel {
     private final Tunable mNumTiles = new Tunable() {
         @Override
         public void onTuningChanged(String key, String newValue) {
-            setMaxTiles(getNumQuickTiles(mContext));
+            setMaxTiles(mDefaultMaxTiles);
         }
     };
 