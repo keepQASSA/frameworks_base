8d7492fd6efce5d08766bcd7a15c6a7a8f18de01
Beverly Tai <beverlyt@google.com>
2023-09-14 20:50:28 +0000

    Revert "On device lockdown, always show the keyguard"
    This reverts commit b23c2d5fb6630ea0da503b937f62880594b13e94.
    
    Reason for revert: b/300463732 regression
    Bug: 300463732
    Bug: 218495634
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f57217125f2b124c16c463ef4507fb054cc1ba4f)
    Merged-In: I31485d0d8caa3060e998636b071dbe03f6b4fc82
    Change-Id: I31485d0d8caa3060e998636b071dbe03f6b4fc82

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -564,13 +564,6 @@ public class KeyguardViewMediator extends SystemUI {
                 notifyHasLockscreenWallpaperChanged(hasLockscreenWallpaper);
             }
         }
-
-        @Override
-        public void onStrongAuthStateChanged(int userId) {
-            if (mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
-                doKeyguardLocked(null);
-            }
-        }
     };
 
     ViewMediatorCallback mViewMediatorCallback = new ViewMediatorCallback() {
@@ -1325,8 +1318,7 @@ public class KeyguardViewMediator extends SystemUI {
         }
 
         // if another app is disabling us, don't show
-        if (!mExternallyEnabled
-            && !mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
+        if (!mExternallyEnabled) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because externally disabled");
 
             mNeedToReshowWhenReenabled = true;
