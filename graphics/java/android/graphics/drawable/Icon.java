xiaomi-sm8450-marble/android_frameworks_base_new

Further reduce notifications bitmap resource usage
* we're having qs panel lags when too many notifications with large bitmaps shows up (like youtube video preview), try compressing the notification bitmaps to webp to reduce memory usage by 40-60% assuming the bitmap is using jpg compression format

graphics/java/android/graphics/drawable/Icon.java
@@ -1150,12 +1150,23 @@ public final class Icon implements Parcelable {
         if (bitmapWidth > maxWidth || bitmapHeight > maxHeight) {
             float scale = Math.min((float) maxWidth / bitmapWidth,
                     (float) maxHeight / bitmapHeight);
-            bitmap = Bitmap.createScaledBitmap(bitmap,
-                    Math.max(1, (int) (scale * bitmapWidth)),
-                    Math.max(1, (int) (scale * bitmapHeight)),
+            int scaledWidth = (int) (scale * bitmapWidth);
+            int scaledHeight = (int) (scale * bitmapHeight);
+            if (scaledWidth > maxWidth) {
+                scaledWidth = maxWidth;
+            }
+            if (scaledHeight > maxHeight) {
+                scaledHeight = maxHeight;
+            }
+            bitmap = Bitmap.createScaledBitmap(bitmap, 
+                    Math.max(1, scaledWidth), 
+                    Math.max(1, scaledHeight), 
                     true /* filter */);
         }
-        return bitmap;
+        java.io.ByteArrayOutputStream byteArrayOutputStream = new java.io.ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSY, 100, byteArrayOutputStream);
+        byte[] compressedBytes = byteArrayOutputStream.toByteArray();
+        return BitmapFactory.decodeByteArray(compressedBytes, 0, compressedBytes.length);
     }
 
     /**

keepQASSA/frameworks_base

graphics/java/android/graphics/drawable/Icon.java
@@ -878,12 +878,23 @@ public final class Icon implements Parcelable {
         if (bitmapWidth > maxWidth || bitmapHeight > maxHeight) {
             float scale = Math.min((float) maxWidth / bitmapWidth,
                     (float) maxHeight / bitmapHeight);
+            int scaledWidth = (int) (scale * bitmapWidth);
+            int scaledHeight = (int) (scale * bitmapHeight);
+            if (scaledWidth > maxWidth) {
+                scaledWidth = maxWidth;
+            }
+            if (scaledHeight > maxHeight) {
+                scaledHeight = maxHeight;
+            }
             bitmap = Bitmap.createScaledBitmap(bitmap,
-                    Math.max(1, (int) (scale * bitmapWidth)),
-                    Math.max(1, (int) (scale * bitmapHeight)),
+                    Math.max(1, scaledWidth),
+                    Math.max(1, scaledHeight),
                     true /* filter */);
         }
-        return bitmap;
+        java.io.ByteArrayOutputStream byteArrayOutputStream = new java.io.ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.WEBP, 100, byteArrayOutputStream);
+        byte[] compressedBytes = byteArrayOutputStream.toByteArray();
+        return BitmapFactory.decodeByteArray(compressedBytes, 0, compressedBytes.length);
     }
 
     /**